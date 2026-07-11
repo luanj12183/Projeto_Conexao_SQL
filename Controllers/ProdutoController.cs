fd44b902a085cd0a50f3c976b0f67f6b1071db3d
LUAN JUSTINIANO DOS SANTOS <luanj12183@gmail.com>
2026-07-11 17:27:15 +0000

    Configuração completa do DbContext com SQLite e Migrations aplicadas

--- Controllers/ProdutoController.cs
+++ Controllers/ProdutoController.cs
@@ -1,23 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
+using Projeto_Conexao_MYSQL.Data; // Importa o seu AppDbContext
 using Projeto_Conexao_MYSQL.Models;
 using System.Collections.Generic;
+using System.Linq; // Permite usar o método .ToList()
 
 namespace Projeto_Conexao_MYSQL.Controllers
 {
-    
     public class ProdutoController : Controller
     {
+        private readonly AppDbContext _context;
+
+        // O ASP.NET Core passa o banco de dados automaticamente para cá (Injeção de Dependência)
+        public class ProdutoController(AppDbContext context) : Controller
+        {
+            _context = context;
+        }
 
         public IActionResult Index()
         {
-
-            List<Produto> ListaProdutos = new List<Produto>()
-            {
-
-            new Produto { Id = 1, NomesProdutos = "Teclado", Estoques = 30, Precos = 400.00 },
-            new Produto { Id = 2, NomesProdutos = "Notebook", Estoques = 50, Precos = 3500.00 },
-            new Produto { Id = 3, NomesProdutos = "Cabo de Rede", Estoques = 60, Precos = 70.50 },
-            };
+            // Busca todos os produtos cadastrados fisicamente no seu banco de dados
+            List<Produto> ListaProdutos = _context.Produtos.ToList();
 
             ViewBag.Produtos = ListaProdutos;
 
---
+++ Migrations/20260711172346_CriarTabelaProdutos.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Projeto_Conexao_MYSQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriarTabelaProdutos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    NomesProdutos = table.Column<string>(type: "TEXT", nullable: false),
+                    Estoques = table.Column<int>(type: "INTEGER", nullable: false),
+                    Precos = table.Column<double>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produtos");
+        }
+    }
+}
